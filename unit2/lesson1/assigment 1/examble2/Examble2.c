3083a724cf33fba080a125dedad9d843f375380c
abdelrhman mohamed zedan <zedanabdelrhman96@gmail.com>
2023-01-31 16:03:46 -0800

    Assignment solutions for the second unit

---
+++ unit2/lesson1/assigment 1/examble2/Examble2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	printf("enter an integr numberes:\r\n");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&x);
+	printf("the number you entered is: %d\r\n",x);
+
+}
